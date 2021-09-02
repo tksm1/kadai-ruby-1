d2ed862ada6ca2c0950263336ee952356b7d9f2d
Ubuntu <ubuntu@ip-172-31-5-204.ap-northeast-1.compute.internal>
2021-09-02 14:00:53 +0000

    commit for kadai-ruby-1

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,9 +1,6 @@
 family_name = '前山'
 name = '武志'
-str = 'です。'
-
 num = 22
-str = '歳です。'
 num.to_s
 
-puts family_name + name + str + num.to_s + str  
+puts family_name + name + 'です。' + num.to_s + '歳です。' 
